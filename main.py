4c12906b69280bbb16cb21ed51d2e14cf4e1b37a
Alexandro Mena <alexandro.mena0108@gmail.com>
2026-02-25 22:31:12 +0800

    add modification for ci/cd

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+app_name = os.getenv("APP_NAME")
 
 @app.get("/")
 async def home():
     return {
-        "message" : "App is  running"
+        "message" : "App is  running",
+        "app" : app_name
     }
